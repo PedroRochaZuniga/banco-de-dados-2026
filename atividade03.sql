5a7e4d2a272c61cf7bc8cc2cf1978a5eeb85c431
Pedro Rocha Zuniga <pedrorzunig4@gmail.com>
2026-04-17 11:05:21 -0300

    Update atividade03.sql

--- atividade03.sql
+++ atividade03.sql
@@ -110,3 +110,24 @@ COMMIT;
 -- Qual a função do FOR UPDATE?
 -- bloqueia sa linhas retornadas pelo select até o fim da transação, evitando conflito de concorrencia
 
+START TRANSACTION;
+UPDATE contas
+SET saldo = saldo - 50
+WHERE id = 1;
+
+
+
+
+session2: SELECT * FROM contas WHERE id = 1;
+
+START TRANSACTION;
+UPDATE contas
+SET saldo = saldo + 300
+WHERE id = 1;
+COMMIT;
+
+
+START TRANSACTION;
+UPDATE contas
+SET saldo = saldo + 70
+WHERE id = 4;
